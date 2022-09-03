5e845b51c920de5c69c6fa5461107c7a75d14bb6
Marcel-Oramas <squallxhola@gmail.com>
2022-09-03 17:33:57 -0300

    tweet virtuals to json

--- models/Tweet.js
+++ models/Tweet.js
@@ -7,6 +7,7 @@ module.exports = (mongoose) => {
       likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     },
     { timestamps: true },
+    { toJSON: { virtuals: true } },
   );
 
   const Tweet = mongoose.model("Tweet", tweetSchema);
